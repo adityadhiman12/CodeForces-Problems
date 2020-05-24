38d05fe99333839b111b3c14732fe2e4d2eb57a3
Aditya Dhiman <adityadhiman12@gmail.com>
2020-05-25 03:04:16 +0530

    one problem under 1100 of number theory

---
+++ 1100/Ebony and Ivory.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+ll gcd(ll a,ll b)
+{
+	if(b==0)
+		return a;
+	return gcd(b,a%b);	
+}
+
+int main()
+{
+	ll a,b,c;
+	cin>>a>>b>>c;
+	int flag=0;
+	for (int i = 0; a * i <= c; ++ i){
+        if ((c - a * i) % b == 0)
+        {
+            printf("Yes\n");
+            return 0;
+        }
+	}
+	
+		cout<<"No"<<"\n";	
+}
