97ddf970e85d3d3d06c9a37fb4990def7991c6c2
Aditya Dhiman <adityadhiman12@gmail.com>
2020-05-27 03:25:44 +0530

    3 questions under number theory

---
+++ 1000/Noldbach problem.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define max 100010
+using namespace std;
+
+int a[max];
+vector<int>v;
+
+int prime()
+{
+	int i,j;
+	memset(a,false,sizeof(a));
+
+	for(i=2;i<=sqrt(max);i++)
+	{
+		if(a[i]==false)
+		{
+		for(j=2*i;j<=max;j=j+i)
+		{
+			a[j]=true;
+		}
+
+		}
+	}
+
+	a[0]=a[1]=true;
+
+	for(i=0;i<=max;i++)
+	{
+		if(a[i]==false)
+			v.push_back(i);
+	}
+
+	return 0;
+}
+
+int main()
+{
+	prime();
+
+	int n,k;
+
+	int i;
+
+
+	scanf("%d %d",&n,&k);
+
+	int count=0;
+
+	for(i=0;v[i]<=n;i++)
+	{
+		int m=v[i]+v[i+1]+1;
+		if(m>n)
+			break;
+		if(a[m]==false)
+		{
+			count++;
+		}
+
+		if(count==k)
+			break;
+	}
+
+	if(count==k)
+		printf("YES\n");
+	else
+		printf("NO\n");
+
+	return 0;
+}
---
+++ 900/A. Almost Prime.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define n 3001
+using namespace std;
+
+int main()
+{
+	bool arr[n];
+	int a[n]={0};
+	for(int i=2;i<n;i++)
+		if(!arr[i])
+			for(int j=2*i;j<n;j+=i)
+			{
+				arr[j]=1;
+				a[j]++;
+			}
+	int m;
+	cin>>m;
+	int res=0;
+	for(int i=1;i<=m;i++)
+	{
+		if(a[i]==2)
+			res++;	
+	}
+	cout<<res<<"\n";
+			
+}
