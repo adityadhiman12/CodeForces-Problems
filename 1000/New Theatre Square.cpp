7863ad700d196d60fae99b7ed9c6c48d1ccda450
Aditya Dhiman <adityadhiman12@gmail.com>
2020-05-29 22:57:44 +0530

    1 problem under 1000

---
+++ 1000/New Theatre Square.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	   int n,m,x,y;
+	   cin>>n>>m>>x>>y;
+	   char a[n][m];
+	   int cost=0;
+	   int p=min(y,2*x);
+	   for(int i=0;i<n;i++)
+	   {
+	       for(int j=0;j<m;j++)
+		   {
+	            cin>>a[i][j];
+		   }
+	   }
+	   for(int i=0;i<n;i++)
+	   {
+	       for(int j=0;j<m-1;j++)
+	       {
+	           if(a[i][j]=='.')
+	            {
+					if(a[i][j+1]=='.')
+					{
+						cost+=p;
+				// 		cout<<cost<<"\n";
+						a[i][j+1]='*';
+					}
+					else
+						cost+=x;
+	            }
+		   }
+		   if(a[i][m-1]=='.')
+				cost+=x;
+	   }
+	   cout<<cost<<"\n";
+	            
+	            
+	   
+	}
+	return 0;
+}
