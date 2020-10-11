ac1fdc1191fb4f513e44db90eaf2eeb791a5b664
Aditya Dhiman <adityadhiman12@gmail.com>
2020-10-11 23:42:55 +0530

    grade edited

--- 600/Grade Allocation.cpp
+++ 600/Grade Allocation.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define ll long long int
+#define endl "\n"
 using namespace std;
 
 int main()
@@ -18,6 +19,6 @@ int main()
             cin>>x;
             s+=x;
         }
-        cout<<min(s,m)<<"\n";
+        cout<<min(s,m)<<endl;
     }
 }
