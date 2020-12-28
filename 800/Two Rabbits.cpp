0364fd4dd18e8a511619367d6c2b3ee12ac39979
Aditya Dhiman <32256939+adityadhiman12@users.noreply.github.com>
2020-12-28 22:27:45 +0530

    Update Two Rabbits.cpp

--- 800/Two Rabbits.cpp
+++ 800/Two Rabbits.cpp
@@ -11,8 +11,8 @@ int main()
 		ll x,y,a,b;
 		cin>>x>>y>>a>>b;
 		if((y-x)%(a+b)==0)
-			cout<<(y-x)/(a+b)<<"\n";
+			cout<<(y-x)/(a+b)<<endl;
 		else
-			cout<<-1<<"\n";	
+			cout<<-1<<endl;	
 	}
 }
