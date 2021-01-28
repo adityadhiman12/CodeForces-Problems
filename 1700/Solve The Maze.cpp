d830f789109900736e2a5cd8eec953ea598c6617
Aditya Dhiman <32256939+adityadhiman12@users.noreply.github.com>
2021-01-28 23:48:14 +0530

    Update Solve The Maze.cpp

--- 1700/Solve The Maze.cpp
+++ 1700/Solve The Maze.cpp
@@ -15,7 +15,8 @@ void dfs(int x,int y)
     if(y<m)dfs(x,y+1);
 }
 int main(){
-	for(cin>>t1;t1--;){
+	for(cin>>t1;t1--;)
+        {
 		cin>>n>>m;
 		for(int i=1;i<=n;i++)scanf("%s",a[i]+1);
 		for(int i=0;i<=n+1;i++) for(int j=0;j<=m+1;j++) b[i][j]=v[i][j]=0;
