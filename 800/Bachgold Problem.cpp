bfb46bc4e43a4c9b262216f46c4d0f6bc3cd117e
Aditya Dhiman <32256939+adityadhiman12@users.noreply.github.com>
2021-04-11 23:59:25 +0530

    Update Bachgold Problem.cpp

--- 800/Bachgold Problem.cpp
+++ 800/Bachgold Problem.cpp
@@ -6,12 +6,14 @@ int main()
 	ll n;
 	cin >> n;
 	
-	if(n % 2 == 0){
+	if(n % 2 == 0)
+        {
 		cout << n / 2 << endl;
 		for(int i = 0; i < n / 2; i++){
 			cout << 2 << ' ';
-		}
-	} else {
+              }
+	} 
+        else {
 		cout << (n - 3) / 2 + 1 << endl;
 		for(int i = 0; i < (n - 3) / 2; i++){
 			cout << 2 << ' ';
