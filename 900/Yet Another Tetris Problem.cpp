f4d3eb2c3adf494432d6e0a555d9923d78666e24
Aditya Dhiman <32256939+adityadhiman12@users.noreply.github.com>
2021-05-16 22:57:55 +0530

    Update Yet Another Tetris Problem.cpp

--- 900/Yet Another Tetris Problem.cpp
+++ 900/Yet Another Tetris Problem.cpp
@@ -10,22 +10,22 @@ int main()
     cin>>t;
     while(t--)
     {
-		ll n;
-		cin>>n;
-		vector<ll>a(n);
-		ll odd=0;
-		ll even=0;
-		for(ll i=0 ; i<n ; i++)
-		{
-			cin>>a[i];
-			if(a[i]&1)
-				odd++;
-			else
-				even++;
-		}		
-		if(odd==n || even==n)
-			cout<<"YES"<<endl;
-		else 
-			cout<<"NO"<<endl;
-	}
+	ll n;
+	cin>>n;
+	vector<ll>a(n);
+	ll odd=0;
+	ll even=0;
+	for(ll i=0 ; i<n ; i++)
+	{
+		cin>>a[i];
+		if(a[i]&1)
+			odd++;
+		else
+			even++;
+	}		
+	if(odd==n || even==n)
+		cout<<"YES"<<endl;
+	else 
+		cout<<"NO"<<endl;
+     }
 }
